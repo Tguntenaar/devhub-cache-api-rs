a396dfdfb1ef985268a9f59bfbbc99805bfda995
Thomas <thomasguntenaar@gmail.com>
2024-10-23 13:27:30 -0500

    add simple api client

---
+++ src/api_client.rs
@@ -0,0 +1,38 @@
+use reqwest::Client;
+use serde::Deserialize;
+use tokio::time::{self, Duration};
+
+#[derive(Deserialize)]
+pub struct ApiResponse {
+    // Define the response fields
+    pub data: String,
+}
+
+#[derive(Clone)]
+pub struct ApiClient {
+    client: Client,
+    api_url: String,
+}
+
+// TODO Create a nearblocks client
+// https://github.com/Nearblocks/nearblocks/tree/main/apps/api/src
+
+impl ApiClient {
+    pub fn new(api_url: &str) -> Self {
+        Self {
+            client: Client::new(),
+            api_url: api_url.to_string(),
+        }
+    }
+
+    pub async fn get_data(&self) -> Result<ApiResponse, reqwest::Error> {
+        let res = self
+            .client
+            .get(&self.api_url)
+            .send()
+            .await?
+            .json::<ApiResponse>()
+            .await?;
+        Ok(res)
+    }
+}
